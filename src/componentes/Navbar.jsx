d5bd5ad71ab0344084b8709abf811b41fccd1f96
Michel Calderon <calderonvanessa0411@gmail.com>
2026-04-24 19:24:59 -0500

    primer commit base-react

---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,7 @@
+function Navbar(){
+    return (<nav>
+        <h2>mi tienda</h2>
+    </nav>);
+} 
+
+export default Navbar;
---
+++ src/componentes/ProductCard.jsx
@@ -0,0 +1,9 @@
+function ProductCard(){
+    return(
+        <section>
+            <h3>producto</h3>
+            <p>$100</p>
+        </section>
+    )
+}
+export default ProductCard;
