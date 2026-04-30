2ff79c9d7ef45e3c2d344f4b005d1bd69f757d0c
crowmaker181 <sebasder181@gmail.com>
2026-04-29 19:50:07 -0500

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,15 @@ function App() {
     { id: 1, title: 'laptop', price: 2000 },
     { id: 2, title: 'teclado', price: 100 },
     { id: 3, title: 'mouse', price: 50 },
+    { id: 4, title: 'monitor', price: 500 },
+    { id: 5, title: 'auriculares', price: 100 }
   ];
+
+  const [cart, setCart] = useState([])
+  const addToCart = (product) => { setCart([...cart, product]) }
   return (
     <>
-    <Navbar/> 
+  <Navbar/> 
   <section>
           
         <section>
@@ -24,132 +29,39 @@ function App() {
           <h3>Cesde bogota</h3>
         </section>
     
-    <button onClick={() => console.log(products)}></button>
-
+    <button onClick={() => console.log(products)}>
+      ver en la consola
+      </button>
+    <br />
     <button onClick={() =>{
       setproducts([
-        { id: 1, title: 'laptop', price: 2000 },
-        { id: 2, title: 'teclado', price: 100 },
-        { id: 3, title: 'mouse', price: 50 },
+        ...products,
         { id: products.length + 1, title: 'nuevo producto', price: 500 },
       ])
-    }}>
-
-
+    }}> 
+    agregar producto
     </button>
+
+
+    
   </section>
     <ProductCard title="laptop" price={2000}/>
     <ProductCard title="teclado" price={100}/>
     <ProductCard title="mouse" price={50}/>
+    <ProductCard title="monitor" price={500}/>
+    <ProductCard title="auriculares" price={100}/>
+    <ProductCard onAdd={() => addToCart(products)} />
+
+    {products.map(product => (
+      <ProductCard
+       key={product.id} 
+      title={product.title} 
+      price={product.price}
+      />
+    ))}
     </>
-    //   <section id="center">
-    //     <div className="hero">
-    //       <img src={heroImg} className="base" width="170" height="179" alt="" />
-    //       <img src={reactLogo} className="framework" alt="React logo" />
-    //       <img src={viteLogo} className="vite" alt="Vite logo" />
-    //     </div>
-    //     <div>
-    //       <h1>Get started</h1>
-    //       <p>
-    //         Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-    //       </p>
-    //     </div>
-    //     <button
-    //       className="counter"
-    //       onClick={() => setCount((count) => count + 1)}
-    //     >
-    //       Count is {count}
-    //     </button>
-    //   </section>
-
-    //   <div className="ticks"></div>
-
-    //   <section id="next-steps">
-    //     <div id="docs">
-    //       <svg className="icon" role="presentation" aria-hidden="true">
-    //         <use href="/icons.svg#documentation-icon"></use>
-    //       </svg>
-    //       <h2>Documentation</h2>
-    //       <p>Your questions, answered</p>
-    //       <ul>
-    //         <li>
-    //           <a href="https://vite.dev/" target="_blank">
-    //             <img className="logo" src={viteLogo} alt="" />
-    //             Explore Vite
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a href="https://react.dev/" target="_blank">
-    //             <img className="button-icon" src={reactLogo} alt="" />
-    //             Learn more
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //     <div id="social">
-    //       <svg className="icon" role="presentation" aria-hidden="true">
-    //         <use href="/icons.svg#social-icon"></use>
-    //       </svg>
-    //       <h2>Connect with us</h2>
-    //       <p>Join the Vite community</p>
-    //       <ul>
-    //         <li>
-    //           <a href="https://github.com/vitejs/vite" target="_blank">
-    //             <svg
-    //               className="button-icon"
-    //               role="presentation"
-    //               aria-hidden="true"
-    //             >
-    //               <use href="/icons.svg#github-icon"></use>
-    //             </svg>
-    //             GitHub
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a href="https://chat.vite.dev/" target="_blank">
-    //             <svg
-    //               className="button-icon"
-    //               role="presentation"
-    //               aria-hidden="true"
-    //             >
-    //               <use href="/icons.svg#discord-icon"></use>
-    //             </svg>
-    //             Discord
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a href="https://x.com/vite_js" target="_blank">
-    //             <svg
-    //               className="button-icon"
-    //               role="presentation"
-    //               aria-hidden="true"
-    //             >
-    //               <use href="/icons.svg#x-icon"></use>
-    //             </svg>
-    //             X.com
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a href="https://bsky.app/profile/vite.dev" target="_blank">
-    //             <svg
-    //               className="button-icon"
-    //               role="presentation"
-    //               aria-hidden="true"
-    //             >
-    //               <use href="/icons.svg#bluesky-icon"></use>
-    //             </svg>
-    //             Bluesky
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </section>
-
-    //   <div className="ticks"></div>
-    //   <section id="spacer"></section>
-    // </>
    
-  );
+  )
 }
 
 export default App;
